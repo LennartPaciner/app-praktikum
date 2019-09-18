4e5fda8878bb04ada7358941c14e3c1714534984
Lennart Paciner <lenpac@web.de>
2019-09-18 11:47:25 +0200

    datenbank erstellt

---
+++ praktikum/app/src/main/java/com/example/praktikum/EinkaufsListe.java
@@ -0,0 +1,83 @@
+package com.example.praktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.FragmentTransaction;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EinkaufsListe extends AppCompatActivity {
+
+    private EditText addItem;
+    private int amount = 1;
+    private SQLiteDatabase database;
+    private Button add;
+    private static final String TAG = "Einkaufsliste";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_einkaufs_liste);
+
+        EinkaufsListeDB einkaufsListe = new EinkaufsListeDB(this);
+        database = einkaufsListe.getWritableDatabase();
+
+        //MainFragment fragment = new MainFragment();
+        //FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        //transaction.replace(R.id.container, fragment, "MainFragment");
+
+
+        addItem = findViewById(R.id.eddittext_item);
+        add = findViewById(R.id.button_add);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addItemDB();
+
+            }
+        });
+
+    }
+
+    public void increase(){
+        amount++;
+
+    }
+
+    public void addItemDB(){
+        if (addItem.getText().toString().trim().length() == 0 || amount == 0){
+            return;
+        }
+        String name = addItem.getText().toString();
+
+        ContentValues cv = new ContentValues();
+        cv.put(DBHelper.GroceryEntry.COLUMN_ID,1);
+        cv.put(DBHelper.GroceryEntry.COLUMN_BARCODE, 12345);
+        cv.put(DBHelper.GroceryEntry.COLUMN_NAME, name);
+        cv.put(DBHelper.GroceryEntry.COLUMN_AMOUNT, 5);
+        cv.put(DBHelper.GroceryEntry.COLUMN_MHD, "2008-11-11");
+        cv.put(DBHelper.GroceryEntry.COLUMN_RESTOCK, 1);
+        database.insert(DBHelper.GroceryEntry.TABLE_NAME1, null, cv);
+        database.insert(DBHelper.GroceryEntry.TABLE_NAME2, null, cv);
+
+        //addItem.getText().clear();
+    }
+
+    public void removeItemDB(long id){
+        database.delete(DBHelper.GroceryEntry.TABLE_NAME1, DBHelper.GroceryEntry._ID + "=" + id, null);
+
+    }
+
+    public void updateItemDB(long id, ContentValues content){
+        database.update(DBHelper.GroceryEntry.TABLE_NAME1, content, DBHelper.GroceryEntry._ID + "=" + id, null);
+        //ContentValues con = new ContentValues();
+        //con.put(DBHelper.GroceryEntry.COLUMN_NAME, "nudel");
+        //con.put(DBHelper.GroceryEntry.COLUMN_AMOUNT, 10);
+        //updateItemDB(4, con);
+    }
+}
---
+++ praktikum/app/src/main/java/com/example/praktikum/EinkaufsListeDB.java
@@ -0,0 +1,47 @@
+package com.example.praktikum;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.praktikum.DBHelper.*;
+
+import java.math.BigInteger;
+import java.sql.Date;
+
+import static java.sql.Types.BIT;
+
+public class EinkaufsListeDB extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "DB23.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public EinkaufsListeDB(Context context) {
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_EINKAUFSLISTE_TABLE = "CREATE TABLE " + GroceryEntry.TABLE_NAME1 + " (" +
+                GroceryEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+ GroceryEntry.COLUMN_BARCODE +
+                " INTEGER NOT NULL, "  +GroceryEntry.COLUMN_NAME + " TEXT NOT NULL, "  +
+                GroceryEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " + GroceryEntry.COLUMN_MHD + " TEXT NOT NULL, " +
+                GroceryEntry.COLUMN_RESTOCK + " INTEGER NOT NULL);";
+        db.execSQL(SQL_CREATE_EINKAUFSLISTE_TABLE);
+
+        final String SQL_CREATE_VORRAT_TABLE = "CREATE TABLE " + GroceryEntry.TABLE_NAME2 + " (" + GroceryEntry.COLUMN_ID +
+                " INTEGER PRIMARY KEY AUTOINCREMENT, " + GroceryEntry.COLUMN_BARCODE +
+                " INTEGER NOT NULL, "  +GroceryEntry.COLUMN_NAME + " TEXT NOT NULL, "  +
+                GroceryEntry.COLUMN_AMOUNT + " INTEGER NOT NULL, " + GroceryEntry.COLUMN_MHD + " TEXT NOT NULL, " +
+                GroceryEntry.COLUMN_RESTOCK + " INTEGER NOT NULL);";
+        db.execSQL(SQL_CREATE_VORRAT_TABLE);
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + GroceryEntry.TABLE_NAME1);
+        db.execSQL("DROP TABLE IF EXISTS " + GroceryEntry.TABLE_NAME2);
+        onCreate(db);
+
+    }
+}
