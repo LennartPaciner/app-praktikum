c909dd9d667b0699aefa361806ed70ae32c5a5fe
Lennart Paciner <lenpac@web.de>
2019-09-18 14:57:39 +0200

    getEinkaufsliste from SQL

--- praktikum/app/src/main/java/com/example/praktikum/EinkaufsListe.java
+++ praktikum/app/src/main/java/com/example/praktikum/EinkaufsListe.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.FragmentTransaction;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class EinkaufsListe extends AppCompatActivity {
 
@@ -17,6 +24,7 @@ public class EinkaufsListe extends AppCompatActivity {
     private SQLiteDatabase database;
     private Button add;
     private static final String TAG = "Einkaufsliste";
+    private EinkaufsListeDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +33,6 @@ public class EinkaufsListe extends AppCompatActivity {
 
         EinkaufsListeDB einkaufsListe = new EinkaufsListeDB(this);
         database = einkaufsListe.getWritableDatabase();
-
-        //MainFragment fragment = new MainFragment();
-        //FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        //transaction.replace(R.id.container, fragment, "MainFragment");
 
         add = findViewById(R.id.button_add);
 
@@ -39,6 +43,8 @@ public class EinkaufsListe extends AppCompatActivity {
 
             }
         });
+        //getProductAll(einkaufsListe.getAllData());
+        getProductAll(einkaufsListe.getAllData());
 
     }
 
@@ -78,4 +84,31 @@ public class EinkaufsListe extends AppCompatActivity {
         //con.put(DBHelper.GroceryEntry.COLUMN_AMOUNT, 10);
         //updateItemDB(4, con);
     }
+
+    public JSONArray getProductAll(Cursor result) {
+        JSONArray resultSet = new JSONArray();
+        StringBuffer buffer = new StringBuffer();
+        if (result != null && result.getCount() > 0) {
+            while (result.moveToNext()) {
+                try {
+                    JSONObject object = new JSONObject();
+                    object.put("id", result.getInt(0));
+                    object.put("barcode", result.getInt(1));
+                    object.put("name", result.getString(2));
+                    object.put("menge", result.getInt(3));
+                    object.put("mhd", result.getString(4));
+                    object.put("restock", result.getInt(5));
+                    resultSet.put(object);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        Toast.makeText(this, resultSet.toString(), Toast.LENGTH_LONG).show();
+        return resultSet;
+    }
+
+
+
 }
