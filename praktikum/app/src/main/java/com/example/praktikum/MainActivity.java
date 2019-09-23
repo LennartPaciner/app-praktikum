09882e20e11e8187ffdf8ddf1807a65e53b0e5e3
Dominik <dofranke@students.uni-mainz.de>
2019-09-23 14:31:54 +0200

    Language settings bug behoben

--- praktikum/app/src/main/java/com/example/praktikum/MainActivity.java
+++ praktikum/app/src/main/java/com/example/praktikum/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Toast backToast;
     private ToggleButton languageButton;
     public String appLanguage;
-m7
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
