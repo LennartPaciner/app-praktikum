c9d3de655df2c9ce1a0301f409f8d3777d6e4649
Maurice Dann <mdann@students.uni-mainz.de>
2019-09-25 11:34:39 +0200

    alles

--- praktikum/app/src/main/java/com/example/praktikum/Cooking.java
+++ praktikum/app/src/main/java/com/example/praktikum/Cooking.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,17 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.sql.SQLData;
 
 public class Cooking extends AppCompatActivity {
     private Button einkaufButton;
@@ -31,14 +26,18 @@ public class Cooking extends AppCompatActivity {
     private Button stockButton;
     private Button homeButton;
     private Button addVorrat;
-    private EinkaufsListe einkaufsListe;
+    public EinkaufsListe einkaufsListe;
+    public EinkaufsListeDB einkaufsListeDB;
+
+    String amount;
+    EditText amountTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cooking);
 
-        EinkaufsListeDB einkaufsListeDB = new EinkaufsListeDB(this);
+        einkaufsListeDB = new EinkaufsListeDB(this);
         EinkaufsListe einkaufsListe2 = new EinkaufsListe();
         SQLiteDatabase database = einkaufsListeDB.getWritableDatabase();
         einkaufsListe = einkaufsListe2;
@@ -85,99 +84,101 @@ public class Cooking extends AppCompatActivity {
                 openStockActivity();
             }
         });
-
-        createCookingView(einkaufsListe2.getProductAll(einkaufsListeDB.getAllData2()));
-
+        createEinkaufsliste();
     }
 
-    public void createCookingView(JSONArray array){
-        final ScrollView SV = findViewById(R.id.SView_Cooking);
-        //All Views needed for Layout copy
-        final TableLayout TL = findViewById(R.id.tableLayout_Cooking);
-        final TableRow TR = findViewById(R.id.rowLayout_Cooking);
-        final LinearLayout LL = findViewById(R.id.LL_Cooking);
-        final FrameLayout FL_Product = findViewById(R.id.FL_Cooking_Product);
-        final FrameLayout FL_Increase = findViewById(R.id.FL_Cooking_increase);
-        final FrameLayout FL_Amount = findViewById(R.id.FL_Cooking_Amount);
-        final FrameLayout FL_Decrease = findViewById(R.id.FL_Cooking_decrease);
-        final TextView TV_Product = findViewById(R.id.columnLayout_Cooking_Product);
-        final TextView TV_Amount = findViewById(R.id.columnLayout_Cooking_Amount);
-        final TextView TV_Increase = findViewById(R.id.TV_Cooking_increase);
-        final TextView TV_Decrease = findViewById(R.id.TV_Cooking_decrease);
-        Toast.makeText(this,"CreateCooking",Toast.LENGTH_LONG).show();
-
-        JSONArray arr = array;
-        for(int i = 0; i < arr.length(); i++) {
-            try {
+    public void createEinkaufsliste(){
+        JSONArray arr = einkaufsListe.getProductAll(einkaufsListeDB.getAllData2());
+        for(int i = 0; i < arr.length(); i++){
+            try{
                 JSONObject object = arr.getJSONObject(i);
                 String name = object.getString("name");
-                String amount = object.getString("menge");
+                amount = object.getString("menge");
+                //Toast.makeText(Cooking.this, name, Toast.LENGTH_LONG).show();
                 final int iD = object.getInt("id");
-                Toast.makeText(this, "Loop" + i, Toast.LENGTH_LONG).show();
-                //Add next Row
-                TableRow new_TableRow = new TableRow(this);
-                LinearLayout new_LinearLayout = new LinearLayout(this);
-                new_LinearLayout.setLayoutParams(LL.getLayoutParams());
 
-                //Add Product
-                FrameLayout product_FL = new FrameLayout(this);
-                TextView product_TV = new TextView(this);
-                product_FL.setLayoutParams(FL_Product.getLayoutParams());
-                product_TV.setLayoutParams(TV_Product.getLayoutParams());
-                product_TV.setText(name);
-                product_FL.addView(product_TV);
-                new_LinearLayout.addView(product_FL);
+                final TableLayout tableLayout = findViewById(R.id.tableLayout1);
+                TableRow rowLayout = findViewById(R.id.rowLayout);
+                LinearLayout linearLayout = findViewById(R.id.linearLayout2);
+                FrameLayout frameLayout = findViewById(R.id.frameLayoutProduct);
+                TextView columnLayout = findViewById(R.id.columnLayout);
 
-                //Add IncreaseButton
-                FrameLayout increase_FL = new FrameLayout(this);
-                Button increase_B = new Button(this);
-                increase_FL.setLayoutParams(FL_Increase.getLayoutParams());
-                increase_B.setLayoutParams(TV_Increase.getLayoutParams());
-                increase_B.setText("+");
-                increase_FL.addView(increase_B);
-                new_LinearLayout.addView(increase_FL);
-                increase_B.setOnClickListener(new View.OnClickListener() {
+                final TableRow neu = new TableRow(this);
+                final LinearLayout neuLL = new LinearLayout(this);
+                neuLL.setLayoutParams(linearLayout.getLayoutParams());
+
+                // add name TV
+                FrameLayout nameFL = new FrameLayout(this);
+                TextView nameTV = new TextView(this);
+                nameFL.setLayoutParams(frameLayout.getLayoutParams());
+                nameTV.setLayoutParams(columnLayout.getLayoutParams());
+                nameTV.setText(name);
+                nameFL.addView(nameTV);
+                neuLL.addView(nameFL);
+
+                // add amount checkBox
+                FrameLayout deleteFL = new FrameLayout(this);
+                Button deleteListe = new Button(this);
+                deleteFL.setLayoutParams(frameLayout.getLayoutParams());
+                deleteListe.setLayoutParams(columnLayout.getLayoutParams());
+                //Drawable minusBtn = getResources().getDrawable(R.drawable.remove_cooking);
+                //deleteListe.setBackground(minusBtn);
+                deleteFL.addView(deleteListe);
+                deleteListe.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        //increase Amount by one
+                        float menge = Float.parseFloat(amount);
+                        menge = menge-1;
+                        amount = Float.toString(menge);
+                        setAmountTV(amount);
+
                     }
                 });
+                neuLL.addView(deleteFL);
 
-                //Add Amount
-                FrameLayout amount_FL = new FrameLayout(this);
-                TextView amount_TV = new EditText(this);
-                amount_FL.setLayoutParams(FL_Amount.getLayoutParams());
-                amount_TV.setLayoutParams(TV_Amount.getLayoutParams());
-                amount_TV.setText(amount);
-                amount_FL.addView(amount_TV);
-                new_LinearLayout.addView(amount_FL);
+                // add amount TV
+                final FrameLayout amountFL = new FrameLayout(this);
+                amountTV = new EditText(this);
+                amountFL.setLayoutParams(frameLayout.getLayoutParams());
+                amountTV.setLayoutParams(columnLayout.getLayoutParams());
+                amountTV.setInputType(2);
+                amountTV.setText(amount);
+                amountFL.addView(amountTV);
+                neuLL.addView(amountFL);
 
-                //Add DecreaseButton
-                FrameLayout decrease_FL = new FrameLayout(this);
-                Button decrease_B = new Button(this);
-                decrease_FL.setLayoutParams(FL_Decrease.getLayoutParams());
-                decrease_B.setLayoutParams(TV_Decrease.getLayoutParams());
-                decrease_B.setBackgroundResource(R.drawable.add);
-                decrease_FL.addView(decrease_B);
-                new_LinearLayout.addView(decrease_FL);
-                decrease_B.setOnClickListener(new View.OnClickListener() {
+                // add qr
+                FrameLayout qrFL = new FrameLayout(this);
+                final Button qrBtn = new Button(this);
+                qrFL.setLayoutParams(frameLayout.getLayoutParams());
+                qrBtn.setLayoutParams(columnLayout.getLayoutParams());
+                //Drawable plusBtn = getResources().getDrawable(R.drawable.add_cooking);
+                //qrBtn.setBackground(plusBtn);
+                qrBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        //decrease amount by one
+                        float menge = Float.parseFloat(amount);
+                        menge = menge+1;
+                        amount = Float.toString(menge);
+                        setAmountTV(amount);
                     }
                 });
+                final int tmp = i;
+                qrFL.addView(qrBtn);
+                neuLL.addView(qrFL);
 
+                neu.addView(neuLL);
+                tableLayout.addView(neu);
 
-                new_TableRow.addView(new_LinearLayout);
-                TL.addView(new_TableRow);
-
-
-            } catch (JSONException e) {
+            }catch (JSONException e){
                 Log.e("Einkaufsliste", e.getMessage());
                 e.printStackTrace();
             }
         }
 
+    }
+
+    public void setAmountTV(String a){
+        amountTV.setText(a);
     }
 
     public void openEinkaufsListe(){
